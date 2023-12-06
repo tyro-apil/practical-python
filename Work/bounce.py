bea4ebdebd85aa8ccca73d862c788575ea85074c
apil <078bct017.apil@pcampus.edu.np>
2023-12-06 08:31:49 +0545

    solved bounce problem

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+iteration = 1
+
+while iteration <= 10:
+    height = round((3/5)*height, 4)
+    print(iteration, height)
+    iteration = iteration + 1
